2203e97cc8e1c13aac985b9e2251fa2440f147a0
Jason T <jason@jasont.dev>
2022-05-23 05:28:12 +1000

    Moved checkForWinner to game-logic.ts

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,18 @@
 <script lang="ts">
   import Board from "./lib/Board.svelte";
+  import {checkForWinner} from "./lib/game-logic";
   let squares = ["", "", "", "", "", "", "", "", ""];
   let currentPlayer = "X";
 
   function handleClick(event: CustomEvent<{ square: number }>) {
     if (squares[event.detail.square] === "") {
-      if (!checkForWinner("X") && !checkForWinner("0")) {
+      if (!checkForWinner(squares, "X") && !checkForWinner(squares, "0")) {
         squares[event.detail.square] = currentPlayer;
         currentPlayer = currentPlayer === "X" ? "O" : "X";
       }
     }
   }
 
-  function checkForWinner(player: string): boolean {
-    function checkRow(n: number): boolean {
-      return (
-        squares[n * 3] === player &&
-        squares[n * 3 + 1] === player &&
-        squares[n * 3 + 2] === player
-      );
-    }
-
-    function checkColumn(n: number): boolean {
-      return (
-        squares[n] === player &&
-        squares[n + 3] === player &&
-        squares[n + 6] === player
-      );
-    }
-
-    function checkDiagonals(): boolean {
-      if (
-        squares[0] === player &&
-        squares[4] === player &&
-        squares[8] === player
-      ) {
-        return true;
-      }
-
-      if (
-        squares[2] === player &&
-        squares[4] === player &&
-        squares[7] === player
-      ) {
-        return true;
-      }
-
-      return false;
-    }
-
-    if (checkRow(0) || checkRow(1) || checkRow(2)) {
-      return true;
-    }
-
-    if (checkColumn(0) || checkColumn(1) || checkColumn(2)) {
-      return true;
-    }
-    return checkDiagonals();
-  }
 </script>
 
 <div class="layout">
